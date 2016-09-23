24e6befbf1000da2cadacb4acd32d0e414013378
Cristian <cdcordobaa@unal.edu.co>
2016-09-23 00:40:08 +0000

    dooing nested agregation within componente_practico-unidad

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -6,6 +6,7 @@ class TestController < ApplicationController
   # GET /programas.json
   def index
     @programas = Programa.all
+    render json: @programas
   end
 
   # GET /programas/1
@@ -25,11 +26,40 @@ class TestController < ApplicationController
   # POST /programas
   # POST /programas.json
   def create
-    @programa = Programa.new
-    @programa.nombre_programa = params[:nombre_programa]
-    @programa.save
+    #@programa = Programa.new
+    #@programa.nombre_programa = params[:nombre_programa]
+    #@programa.save
+    programa_hash = { }
     
-    render json: params
+    programa_hash[:nombre_programa] = params[:programa][:nombre_programa]
+    programa_hash[:tipo_educacion] = params[:tipo_educacion]
+    programa_hash[:titulo_otorga] = params[:titulo_otorga]
+    
+    parent_hash = {  }
+    parent_hash[:programa] = params[:programa]
+    
+    instalacion_hash = params.permit( [ {instalacion: :aulas_fisico_espacial} ] )
+    #instalacion_hash = params[:instalacion][:aulas_fisico_espacial]
+    
+    mod_hash = params.permit(  modulo: [{ nombre: :nombre, horas_modulo: :horas_modulo, descripcion: :descripcion }])
+    
+    @componente_teorico = ComponenteTeorico.new(componente_teorico_params)
+    @componente_practico = ComponentePractico.new(componente_practico_params)
+    
+    @componente_practico.modulos = modulo_params.map  do |m|
+      
+      @modulo =  Modulo.new(m.symbolize_keys)
+      
+      #@modulo.unidads = unidad_params.map do |u|
+      #  @unidad = Unidad.new(u.symbolize_keys)
+      #end
+      
+    end
+
+    @componente_practico.save
+    
+    render json: modulo_params.map
+    
   end
 
   # PATCH/PUT /programas/1
@@ -61,10 +91,29 @@ class TestController < ApplicationController
     def set_programa
       @programa = Programa.find(params[:id])
     end
-
+    
     # Never trust parameters from the scary internet, only allow the white list through.
     def programa_params
       params.require(:programa).permit(:nombre_programa, :tipo_educacion, :titulo_otorga, :tiempo_formacion, :familia_profesional)
+    end
+    
+    def componente_teorico_params
+      params.require(:componente_teorico).permit(:nombre_teorico, :total_horas)
+    end
+    
+    def modulo_params
+      #params.permit( [{ modulo: { nombre: :nombre, horas_modulo: :horas_modulo, descripcion: :descripcion }}])
+      #params.permit( [ {instalacion: :aulas_fisico_espacial} ] )
+      params.fetch(:modulo)  
+    end
+    
+    def componente_practico_params
+      params.require(:componente_practico).permit(:nombre_practico, :total_horas)
+    end
+    
+    def unidad_params
+      #params.require(:unidad).permit(:nombre_unidad, :competencias, :horas_unidad)
+      params.fetch(:unidad)
     end
     
 end
